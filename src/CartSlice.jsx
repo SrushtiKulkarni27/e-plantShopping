6cdbacbf125b1262c5b7577e0196590e660fdf98
Srushti Kulkarni <SrushtiKulkarni27>
2025-04-01 07:34:12 -0400

    Initial commit with Product List functionality

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,7 +7,7 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        state.items.push(action.payload); // Add the plant to the items array
     },
     removeItem: (state, action) => {
     },
